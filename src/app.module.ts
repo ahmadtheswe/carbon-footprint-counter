3667e71a213d4e074dcea5b0665cec05c220a486
ahmad <ahmadtheswe@gmail.com>
2025-09-11 08:21:42 +0700

    remove prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CalculationController } from './calculation/calculation.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CalculationController],
   providers: [AppService],
 })
 export class AppModule {}
